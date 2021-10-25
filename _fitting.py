9ca8bee8f8d28a0d6ee42bc38821043f8526fc8d
Mehdi Samsami <mehdi@techclass.fi>
2021-10-26 00:34:38 +0330

    Added fit method to this file

---
+++ _fitting.py
@@ -0,0 +1,25 @@
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def fit(self, decode_error='strict', strip_accents='ascii', lowercase=True, preprocessor=None, tokenizer=None,
+        stop_words=None, analyzer='word', norm='l2',
+        max_df=1.0, min_df=1, use_idf=True, smooth_idf=True, sublinear_tf=False,
+        n_features=1048576, alternate_sign=True):
+    if self.model_name == 'tfidf':
+        self.model = TfidfVectorizer(decode_error=decode_error, strip_accents=strip_accents, lowercase=lowercase,
+                                     norm=norm, preprocessor=preprocessor, tokenizer=tokenizer, stop_words=stop_words,
+                                     analyzer=analyzer, max_df=max_df, min_df=min_df, use_idf=use_idf,
+                                     smooth_idf=smooth_idf, sublinear_tf=sublinear_tf)
+
+        self.ref_embed = self.model.fit_transform(self.q)
+        self.is_fitted_ = True
+
+    elif self.model_name == 'murmurhash3':
+        self.model = HashingVectorizer(decode_error=decode_error, strip_accents=strip_accents, lowercase=lowercase,
+                                       norm=norm, preprocessor=preprocessor, tokenizer=tokenizer, stop_words=stop_words,
+                                       analyzer=analyzer, n_features=n_features, alternate_sign=alternate_sign)
+
+        self.ref_embed = self.model.fit_transform(self.q)
+        self.is_fitted_ = True
+
