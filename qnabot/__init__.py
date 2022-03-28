17beca2ed2750d5cd63da58e464b1bcc3f11212d
Mehdi Samsami <mehdisamsami@live.com>
2022-03-28 13:25:22 +0430

    Fix kb_dir property issue

--- qnabot/__init__.py
+++ qnabot/__init__.py
@@ -1,25 +1,15 @@
 import json
-import os
+import subprocess
 from ._fitting import fit
 from ._prediction import find_similarity, answer
 from typing import Tuple, Union
 
 
 class QnABot:
-    def __init__(self, kb_dir: str = 'knowledge_base.json', model: str = 'tfidf') -> None:
-        # Save knowledge base general info
+    def __init__(self, model: str = 'tfidf', kb_dir: str = 'qnabot/knowledge_base.json') -> None:
+        # Initialize knowledge base
+        self._kb_dir, self.data = None, dict()
         self.kb_dir = kb_dir
-        self.kb_name = self.data['info']['name']
-        self.kb_version = self.data['info']['version']
-        self.kb_author = self.data['info']['author']
-
-        # Save knowledge base details
-        self.no_answers = self.data['no_answer']
-        self.q = [item['q'] for item in self.data['qna']]
-        self.q_idx = []
-        for i, item in enumerate(self.q):
-            self.q_idx.extend([i for j in range(len(item))])
-        self.q = [item for elem in self.q for item in elem]
 
         # Save model-related details
         self.model_name = model
@@ -29,17 +19,28 @@ class QnABot:
 
     @property
     def kb_dir(self):
-        return self.kb_dir
+        return self._kb_dir
 
     @kb_dir.setter
     def kb_dir(self, kb_dir):
-        # Load the knowledge base
         try:
-            with open(self.kb_dir) as file:
+            # Load and initialize knowledge base
+            with open(kb_dir) as file:
                 self.data = json.load(file)
+            self._kb_dir = kb_dir
+            self.kb_name = self.data['info']['name']
+            self.kb_version = self.data['info']['version']
+            self.kb_author = self.data['info']['author']
+
+            # Save knowledge base details
+            self.no_answers = self.data['no_answer']
+            self.q = [item['q'] for item in self.data['qna']]
+            self.q_idx = []
+            for i, item in enumerate(self.q):
+                self.q_idx.extend([i for _ in range(len(item))])
+            self.q = [item for elem in self.q for item in elem]
         except:
             raise FileNotFoundError('Knowledge base file cannot be found')
-        self.kb_dir = kb_dir
 
     def fit(self, *args, **kwargs):
         return fit(self, *args, **kwargs)
@@ -52,7 +53,4 @@ class QnABot:
         return answer(self, input=input, min_score=min_score, return_score=return_score)
 
     def knowledge_base(self) -> None:
-        os.system(f"streamlit run qnabot/knowledge_base.py {self.kb_dir}")
-
-
-
+        subprocess.run(f"streamlit run qnabot/knowledge_base.py {self.kb_dir}", shell=True, check=True)
