652c40875f5585b69c0419f7e46a0364345e97ed
Mehdi Samsami <mehdisamsami@live.com>
2023-05-01 05:21:24 +0330

    major modification and improvement in the structure of knowledge base class

--- qnabot/knowledge_base.py
+++ qnabot/knowledge_base.py
@@ -1,169 +1,173 @@
 import json
 import subprocess
-from typing import Union, List, Optional
+from typing import List
 
-from ._utils import check_value_error, check_type_error
+from ._types import FilePath, QnA, QnAKBMapping, QnAKBMappingExtra
+from ._utils import check_type_error
 from ._exceptions import KnowledgeBaseSchemaError
-from .const import (
-    KNOWLEDGE_BASE_TYPES,
-    DEFAULT_KNOWLEDGE_BASE_TYPE,
-    KNOWLEDGE_BASE_EDITOR_FILE_PATH
-)
+from .const import KNOWLEDGE_BASE_EDITOR_FILE_PATH
 
 
 class QnAKnowledgeBase:
-    name: Optional[str] = None  # Knowledge base name
-    version: Optional[str] = None  # Knowledge base version
-    author: Optional[str] = None  # Knowledge base author name
-    file_path: Optional[str] = None  # Knowledge base file path
+    _is_loaded: bool = False
 
-    _buffer_data: dict = {
-        'data': None,
-        'no_answers': None,
-        'ref_questions': None,
-        'ref_questions_idx': None
+    _info = {
+        "name": None,
+        "version": None,
+        "author": None
     }
 
-    def __init__(self, data: Union[str, dict], type: str = DEFAULT_KNOWLEDGE_BASE_TYPE,  buffer: bool = False):
-        """Initializes an instance of the class based on a given knowledge base information.
+    _cache_data: QnAKBMappingExtra = {
+        "qna": None,
+        "idk_answers": None,
+        "ref_questions": None,
+        "ref_questions_idx": None
+    }
+
+    def __init__(self, filepath_or_buffer: FilePath, cache: bool = False):
+        """Initializes an instance of the class for a given knowledge base.
 
         Args:
-            data (Union[str, dict]): Knowledge base file path (str) or buffer, i.e., the variable containing the
-                                     knowledge base information (dict).
-            type (str): Knowledge base type. Defaults to DEFAULT_KNOWLEDGE_BASE_TYPE.
-            buffer (bool): Whether to store the knowledge base information in the memory instead of loading it
-                           from file each time it's required. Defaults to False.
+            filepath_or_buffer (FilePath): Path to the knowledge base JSON file.
+            cache (bool): Whether to cache the entire knowledge base in memory. Defaults to False.
 
         Returns:
             self: The instance itself.
         """
-        check_value_error('type', type, KNOWLEDGE_BASE_TYPES)
-        if (not buffer) and (not isinstance(data, str)):
-            raise ValueError("buffer must be enabled when data is not a file path")
+        self.filepath_or_buffer = filepath_or_buffer
+        self.cache = cache
 
-        self.type: str = type
-        self.buffer: bool = buffer
+    def _load(self, filepath_or_buffer: FilePath) -> QnAKBMapping:
+        with open(filepath_or_buffer, "r") as file:
+            kb: dict = json.load(file)
 
-        if isinstance(data, str):
-            self.file_path: str = data
-        self._internal_init(data)
+        check_kb_schema(kb)
+
+        self._set_info(kb["info"])
+
+        if self.cache:
+            ref_questions, ref_questions_idx = self._ref_questions(kb["qna"])
+            _cache_data: QnAKBMappingExtra = {
+                "qna": kb["qna"],
+                "idk_answers": kb["idk_answers"],
+                "ref_questions": ref_questions,
+                "ref_questions_idx": ref_questions_idx
+            }
+
+        self._is_loaded = True
+
+        return dict(qna=kb["qna"], idk_answers=kb["idk_answers"])
+
+    def _set_info(self, info: dict):
+        self._info["name"] = info.get('name')
+        self._info["version"] = info.get('version')
+        self._info["name"] = info.get('author')
 
     @staticmethod
-    def _check_schema(data: dict, type: str = DEFAULT_KNOWLEDGE_BASE_TYPE):
-        main_keys = data.keys()
-
-        if type == 'default':
-            for key in ['info', 'no_answer', 'qna']:
-                if key not in main_keys:
-                    raise KnowledgeBaseSchemaError(f"Knowledge base must have an '{key}' field")
-
-            if 'name' not in data['info'].keys():
-                raise KnowledgeBaseSchemaError(f"'name' is not provided in 'info'")
-
-            for key in data['info'].keys():
-                check_type_error(key, data['info'][key], 'str')
-
-            check_type_error('no_answer', data['no_answer'], ['str', ])
-            check_type_error('qna', data['qna'], [{"q": [], "a": []}, ])
-
-        else:
-            pass
-
-    def _load(self, data: Union[str, dict], only_data: bool = False, only_no_answers: bool = False,
-              only_ref_questions: bool = False):
-        """Loads a QnA knowledge from file or Python dictionary.
-
-        Args:
-            data (Union[str, dict]): Knowledge base file path (str) or buffer, i.e., the variable containing the
-                                     knowledge base information (dict).
-        """
-        if type(data) is str:
-            with open(data) as file:
-                data: dict = json.load(file)
-
-        self._check_schema(data)
-
-        if only_data:
-            return data
-
-        no_answers = data['no_answer']
-        if only_no_answers:
-            return no_answers
-
-        ref_questions_unpacked = [item['q'] for item in data['qna']]
+    def _ref_questions(self, qna: List[QnA]):
+        ref_questions_unpacked = [item['q'] for item in qna]
         ref_questions = [item for elem in ref_questions_unpacked for item in elem]
-        if only_ref_questions:
-            return ref_questions
 
         ref_questions_idx = list()
         for i, item in enumerate(ref_questions_unpacked):
-            ref_questions_idx.extend([i for _ in range(len(item))])
+            ref_questions_idx.extend([i] * len(item))
 
-        return data, no_answers, ref_questions, ref_questions_idx
-
-    def _internal_init(self, data: Union[str, dict]):
-        data, no_answers, ref_questions, ref_questions_idx = self._load(data)
-
-        self.name = data['info']['name']
-        self.version = data['info']['version'] if 'version' in data['info'].keys() else None
-        self.author = data['info']['author'] if 'author' in data['info'].keys() else None
-
-        if self.buffer:
-            self._buffer_data['data'] = data
-            self._buffer_data['no_answers'] = no_answers
-            self._buffer_data['ref_questions'] = ref_questions
-            self._buffer_data['ref_questions_idx'] = ref_questions_idx
+        return ref_questions, ref_questions_idx
 
     @property
-    def data(self) -> dict:
-        """Returns the entire knowledge base data.
+    def name(self) -> str:
+        """Returns the name of the knowledge base.
 
         """
-        if self.buffer:
-            return self._buffer_data['data']
-        else:
-            return self._load(data=self.file_path, only_data=True)
+        return self._info["name"]
 
     @property
-    def no_answers(self) -> List[str]:
-        """Returns the list of "I don't understand" answers.
+    def author(self) -> str:
+        """Returns the name of the knowledge base author.
 
         """
-        if self.buffer:
-            return self._buffer_data['no_answers']
+        return self._info["author"]
+
+    @property
+    def version(self) -> str:
+        """Returns the name of the knowledge base.
+
+        """
+        return self._info["version"]
+
+    @property
+    def qna(self) -> List[QnA]:
+        """Returns the list of questions and answers in the knowledge base.
+
+        """
+        if self.cache and self._is_loaded:
+            return self._cache_data['qna']
         else:
-            return self._load(data=self.data, only_no_answers=True)
+            return self._load(filepath_or_buffer=self.filepath_or_buffer)["qna"]
+
+    @property
+    def idk_answers(self) -> List[str]:
+        """Returns the list of "I don't know" answers in the knowledge base.
+
+        """
+        if self.cache and self._is_loaded:
+            return self._cache_data['idk_answers']
+        else:
+            return self._load(filepath_or_buffer=self.filepath_or_buffer)["idk_answers"]
 
     @property
     def ref_questions(self) -> List[str]:
-        """Returns the list of reference questions.
+        """Returns the list of reference questions in the knowledge base.
 
         """
-        if self.buffer:
-            return self._buffer_data['ref_questions']
+        if self.cache and self._is_loaded:
+            return self._cache_data['ref_questions']
         else:
-            return self._load(data=self.data, only_ref_questions=True)
+            return self._ref_questions(self._load(filepath_or_buffer=self.filepath_or_buffer)["qna"])[0]
 
     @property
     def ref_questions_idx(self) -> List[int]:
-        """Returns the list of reference questions' indices.
+        """Returns the list of indices of the reference questions in the knowledge base.
 
         """
-        if self.buffer:
-            return self._buffer_data['ref_questions_idx']
+        if self.cache and self._is_loaded:
+            return self._cache_data['ref_questions_idx']
         else:
-            _, _, _, _ref_questions_idx = self._load(data=self.data)
-            return _ref_questions_idx
+            return self._ref_questions(self._load(filepath_or_buffer=self.filepath_or_buffer)["qna"])[1]
 
     def run_editor(self):
         """Opens the knowledge base editor app in the web browser.
 
         Applicable only when self.file_path is a valid knowledge base file path.
         """
-        editor(self.file_path)
+        editor(self.filepath_or_buffer)
 
     def __repr__(self):
-        return "<QnAKnowledgeBase (name='%s', type='%s', buffer=%s)>" % (self.name, self.type, self.buffer)
+        return "<QnAKnowledgeBase (name='%s', author='%s', version=%s)>" % (self.name, self.author, self.version)
+
+
+def check_kb_schema(data: dict):
+    """Checks the schema of a knowledge base and raises error if it doesn't match the correct schema of QnABot
+    knowledge base.
+
+    Args:
+        data (dict): A dictionary containing knowledge base data.
+
+    """
+    main_keys = data.keys()
+
+    for key in ['info', 'idk_answers', 'qna']:
+        if key not in main_keys:
+            raise KnowledgeBaseSchemaError(f"Knowledge base must have an '{key}' field")
+
+    if 'name' not in data['info'].keys():
+        raise KnowledgeBaseSchemaError(f"'name' is not provided in 'info'")
+
+    for key in data['info'].keys():
+        check_type_error(key, data['info'][key], 'str')
+
+    check_type_error('idk_answers', data['idk_answers'], ['str', ])
+    check_type_error('qna', data['qna'], [{"q": [], "a": []}, ])
 
 
 def editor(file_path: str) -> None:
