c5ed14e1d94a1c45be59af6ba9a5c7bce176a17e
Mehdi Samsami <mehdisamsami@live.com>
2023-05-01 06:09:43 +0330

    import default knowledge base file path

--- qnabot/kb/__init__.py
+++ qnabot/kb/__init__.py
@@ -1,8 +1,10 @@
 __all__ = [
     "QnAKnowledgeBase",
     "FilePath",
-    "QnA"
+    "QnA",
+    "DEFAULT_KNOWLEDGE_BASE_FILE_PATH"
 ]
 
+from ._const import DEFAULT_KNOWLEDGE_BASE_FILE_PATH
 from ._types import FilePath, QnA
 from .knowledge_base import QnAKnowledgeBase
