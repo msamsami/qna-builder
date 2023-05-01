22e3ae169038317494ea318fa45daed5edac5c4c
Mehdi Samsami <mehdisamsami@live.com>
2023-05-01 05:22:30 +0330

    rename default knowledge base, change schema

--- qnabot/const.py
+++ qnabot/const.py
@@ -3,7 +3,7 @@ import os
 
 KNOWLEDGE_BASE_TYPES = ['default', ]
 DEFAULT_KNOWLEDGE_BASE_TYPE = 'default'
-DEFAULT_KNOWLEDGE_BASE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "knowledge_base.json")
+DEFAULT_KNOWLEDGE_BASE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "kb_default.json")
 KNOWLEDGE_BASE_EDITOR_FILE_PATH = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     "knowledge_base_editor.py"
