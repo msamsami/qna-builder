df00eb7e13bb30115464d6f1421e23cdcef1768e
Mehdi Samsami <mehdisamsami@live.com>
2023-05-01 06:03:17 +0330

    add types to module init

--- qnabot/kb/__init__.py
+++ qnabot/kb/__init__.py
@@ -1,3 +1,8 @@
-__all__ = ['QnAKnowledgeBase']
+__all__ = [
+    "QnAKnowledgeBase",
+    "FilePath",
+    "QnA"
+]
 
+from ._types import FilePath, QnA
 from .knowledge_base import QnAKnowledgeBase
